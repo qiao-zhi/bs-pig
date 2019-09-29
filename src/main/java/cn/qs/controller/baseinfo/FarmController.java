e67fc767bf107b9078c78763435fe9befa84c9cb
qiao_zhi <qiao_liqiang@163.com>
2019-09-29 19:04:58 +0800

    xxx

--- src/main/java/cn/qs/controller/baseinfo/FarmController.java
+++ src/main/java/cn/qs/controller/baseinfo/FarmController.java
@@ -3,8 +3,15 @@ package cn.qs.controller.baseinfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.qs.controller.AbstractController;
+
 @Controller
 @RequestMapping("farm")
-public class FarmController {
+public class FarmController extends AbstractController {
+
+	@Override
+	public String getViewBasePath() {
+		return "farm";
+	}
 
 }
--- src/main/java/cn/qs/controller/baseinfo/PigletController.java
+++ src/main/java/cn/qs/controller/baseinfo/PigletController.java
@@ -3,8 +3,15 @@ package cn.qs.controller.baseinfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.qs.controller.AbstractController;
+
 @Controller
 @RequestMapping("piglet")
-public class PigletController {
+public class PigletController extends AbstractController {
+
+	@Override
+	public String getViewBasePath() {
+		return "piglet";
+	}
 
 }
